e3c903ac00a79c8e4b59f14e930b0132f010c771
dungtt <trinhtiendung54@gmail.com>
2024-02-07 03:06:51 +0700

    fix response api

--- src/main/java/com/project/soshuceapi/common/ResponseMessage.java
+++ src/main/java/com/project/soshuceapi/common/ResponseMessage.java
@@ -7,19 +7,18 @@ public class ResponseMessage {
         public static final String SUCCESS = "%.success";
         public static final String FAIL = "%.fail";
         public static final String ERROR = "%.error";
-        public static final String NOT_FOUND = "%.not-found";
+        public static final String NOT_FOUND = "%.not.found";
         public static final String EXISTED = "%.existed";
         public static final String EMPTY = "%.empty";
-        public static final String NOT_MATCH = "%.not-match";
+        public static final String NOT_MATCH = "%.not.match";
         public static final String INVALID_INPUT = "%.invalid.input";
     }
 
     public static class Authentication {
-        public static final String INVALID_CREDENTIALS = "%.invalid-credentials";
-        public static final String AUTHENTICATION_ERROR = "%.authentication-error";
-        public static final String VERIFY_CODE_EXPIRED = "%.verify-code-expired";
-        public static final String VERIFY_CODE_INCORRECT = "%.verify-code-incorrect";
-        public static final String PERMISSION_DENIED = "%.permission-denied";
+        public static final String AUTHENTICATION_ERROR = "%.authentication.error";
+        public static final String VERIFY_CODE_EXPIRED = "%.verify.code.expired";
+        public static final String VERIFY_CODE_INCORRECT = "%.verify.code.incorrect";
+        public static final String PERMISSION_DENIED = "%.permission.denied";
     }
 
 }
