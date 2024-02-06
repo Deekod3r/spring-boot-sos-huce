9b968783c8e3ae522dbc52afdd302a33603fafbb
dungtt <trinhtiendung54@gmail.com>
2024-02-07 02:55:21 +0700

    fix response api

--- src/main/java/com/project/soshuceapi/common/ResponseCode.java
+++ src/main/java/com/project/soshuceapi/common/ResponseCode.java
@@ -3,22 +3,19 @@ package com.project.soshuceapi.common;
 public class ResponseCode {
     
     public static class Common {
-        public static final String SUCCESS = "00";
-        public static final String FAIL = "01";
-        public static final String NOT_FOUND = "02";
-        public static final String EXISTED = "03";
-        public static final String INVALID = "04";
-        public static final String NOT_MATCH = "05";
-        public static final String EMPTY = "06";
-
+        public static final String SUCCESS = "000";
+        public static final String FAIL = "001";
+        public static final String NOT_FOUND = "002";
+        public static final String EXISTED = "003";
+        public static final String INVALID = "004";
+        public static final String NOT_MATCH = "005";
     }
     
     public static class Authentication {
-        public static final String INVALID_CREDENTIALS = "05";
-        public static final String AUTHENTICATION_ERROR = "06";
-        public static final String VERIFY_CODE_EXPIRED = "07";
-        public static final String VERIFY_CODE_INCORRECT = "08";
-        public static final String PERMISSION_DENIED = "09";
+        public static final String AUTHENTICATION_ERROR = "100";
+        public static final String VERIFY_CODE_EXPIRED = "101";
+        public static final String VERIFY_CODE_INCORRECT = "102";
+        public static final String PERMISSION_DENIED = "103";
     }
     
 }
